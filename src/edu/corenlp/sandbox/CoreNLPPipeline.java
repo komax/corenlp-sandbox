dc9949b40a4bf72835d73b0ebfd78e4a87b8335d
Maximilian Konzack <maximilian.konzack@idiv.de>
2018-09-03 09:01:50 +0200

    Moved common code to Utils

--- src/edu/corenlp/sandbox/CoreNLPPipeline.java
+++ src/edu/corenlp/sandbox/CoreNLPPipeline.java
@@ -2,32 +2,18 @@ package edu.corenlp.sandbox;
 
 import edu.stanford.nlp.pipeline.CoreDocument;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
-import edu.stanford.nlp.pipeline.XMLOutputter;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Properties;
 
 public class CoreNLPPipeline {
-    public static String readTextfile(String path) throws IOException {
-        var lines = Files.readAllLines(Paths.get(path));
-        return lines.stream().reduce("", String::concat);
-    }
-
-    public static void writeXMLfromDocument(String outputFile, CoreDocument doc, StanfordCoreNLP pipeline) throws IOException {
-        var xmlDoc = XMLOutputter.annotationToDoc(doc.annotation(), pipeline);
-        var xml = xmlDoc.toXML().getBytes();
-        var path = Paths.get(outputFile);
-        Files.write(path, xml);
-    }
 
     public static void main(String[] args) throws IOException{
         if (args.length < 1) {
             throw new IllegalArgumentException("Need to supply a path to a text file as an argument");
         }
         // Read the text file.
-        var text = readTextfile(args[0]);
+        var text = Utils.readTextfile(args[0]);
 
         // Store set up for the pipeline as properties.
         var props = new Properties();
@@ -56,7 +42,7 @@ public class CoreNLPPipeline {
 
         // Write an xml file from the annotated document.
         var xmlDocPath = "./doc.xml";
-        writeXMLfromDocument(xmlDocPath, document, pipeline);
+        Utils.writeXMLfromDocument(xmlDocPath, document, pipeline);
 
 
     }
---
+++ src/edu/corenlp/sandbox/Utils.java
@@ -0,0 +1,24 @@
+package edu.corenlp.sandbox;
+
+import edu.stanford.nlp.pipeline.CoreDocument;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.pipeline.XMLOutputter;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Utils {
+    public static String readTextfile(String path) throws IOException {
+        var lines = Files.readAllLines(Paths.get(path));
+        return lines.stream().reduce("", String::concat);
+    }
+
+    public static void writeXMLfromDocument(String outputFile, CoreDocument doc, StanfordCoreNLP pipeline) throws IOException {
+        var xmlDoc = XMLOutputter.annotationToDoc(doc.annotation(), pipeline);
+        var xml = xmlDoc.toXML().getBytes();
+        var path = Paths.get(outputFile);
+        Files.write(path, xml);
+    }
+
+}
