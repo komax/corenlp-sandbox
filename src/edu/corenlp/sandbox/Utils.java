a6412d76f07989c32674e911d7748224dd4b357c
Maximilian Konzack <maximilian.konzack@idiv.de>
2019-02-07 15:02:22 +0100

    updated xml output to latest corenlp api and resolving path names

--- src/edu/corenlp/sandbox/Utils.java
+++ src/edu/corenlp/sandbox/Utils.java
@@ -4,14 +4,17 @@ import edu.stanford.nlp.pipeline.CoreDocument;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
 import edu.stanford.nlp.pipeline.XMLOutputter;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Utils {
-    public static String readTextfile(String path) throws IOException {
-        return readTextfile(Paths.get(path));
+    public static String readTextfile(String filename) throws IOException {
+        filename = filename.replaceFirst("^~", System.getProperty("user.home"));
+        var path = Paths.get(filename);
+        return readTextfile(path);
     }
 
     public static String readTextfile(Path path) throws IOException {
@@ -20,10 +23,12 @@ public class Utils {
     }
 
     public static void writeXMLfromDocument(String outputFile, CoreDocument doc, StanfordCoreNLP pipeline) throws IOException {
-        var xmlDoc = XMLOutputter.annotationToDoc(doc.annotation(), pipeline);
-        var xml = xmlDoc.toXML().getBytes();
-        var path = Paths.get(outputFile);
-        Files.write(path, xml);
+        //Document xmlDoc = XMLOutputter.annotationToDoc(doc.annotation(), pipeline);
+        //var xml = xmlDoc.toXML().getBytes();
+        //var path = Paths.get(outputFile);
+        var outputStream = new FileOutputStream(outputFile);
+        XMLOutputter.xmlPrint(doc.annotation(), outputStream);
+        //Files.write(path, xml);
     }
 
 }
